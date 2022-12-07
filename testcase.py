fe97818daf878d1d1c70380a3dc5014e773b510c
quangtran.sdb <quangtran.sdb@outlook.com>
2022-12-07 09:09:55 +0700

    tests

--- testcase.py
+++ testcase.py
@@ -1,3 +1,15 @@
-from model import Parser
+from model import Parser, Database
 
-p = Parser("Máy bay nào bay từ Đà Nẵng đến TP. Hồ Chí Minh mất 1 giờ ?.")
+
+if __name__ == "__main__":
+    # Initialization
+    db = Database("./input/database.db")
+    # fp = open("./input/query.csv")
+    with open("./input/query.csv",'r', encoding="utf-8") as fp:
+        lines = fp.readlines()
+        for line in lines:
+            p = Parser(line, db)
+            p.do_MaltParser()
+            p.AnalysisGrammarRelationTree(f"./output/output{lines.index(line)}.txt")
+
+    p = Parser("Máy bay nào bay từ Đà Nẵng đến TP. Hồ Chí Minh mất 1 giờ ?.")
